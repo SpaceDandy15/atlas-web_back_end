dc76674b37fb9da3ad38581e8edd188fbd6a415a
Malik Vance <malikovance@gmail.com>
2025-03-04 13:11:08 -0600

    two more issues fixed.

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,9 +4,9 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    return {
+  return {
     [`income-${getCurrentYear()}`]: income,
     [`gdp-${getCurrentYear()}`]: gdp,
     [`capita-${getCurrentYear()}`]: capita,
-    };
+  };
 }
