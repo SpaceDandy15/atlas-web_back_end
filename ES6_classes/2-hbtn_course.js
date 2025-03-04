354d60c89e083ef4ed0d7270584cdaed6409b0eb
Malik Vance <malikovance@gmail.com>
2025-03-04 14:57:03 -0600

    lint

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -2,11 +2,11 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
-    }
+    }``
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    if (!Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+    if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
 
@@ -42,7 +42,7 @@ export default class HolbertonCourse {
   }
 
   set students(value) {
-    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
+    if (!Array.isArray(value) || value.some(student => typeof student !== 'string')) {
       throw new TypeError('Students must be an array of strings');
     }
     this._students = value;
