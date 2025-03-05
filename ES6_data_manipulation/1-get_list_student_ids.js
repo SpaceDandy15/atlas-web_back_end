2ab4e9b63d3eebc5db5721e5820702889b1f3349
Malik Vance <malikovance@gmail.com>
2025-03-05 15:57:50 -0600

    linterrrr

--- ES6_data_manipulation/1-get_list_student_ids.js
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,9 +1,9 @@
 function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
+  if (!Array.isArray(students)) {
     return [];
   }
   // Use the map function to extract the 'id' from each student object
-  return students.map(student => student.id);
+  return students.map((student) => student.id);
 }
 
 export default getListStudentIds;
