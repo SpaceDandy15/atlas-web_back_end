f31b375b42870821345195a8b51be51cfd4da76e
Malik Vance <malikovance@gmail.com>
2025-03-05 13:57:08 -0600

    added getter method to access the Symbol properties from the Car class. with comments!

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,46 +1,36 @@
-export class HolbertonClass {
-  constructor(year, location) {
-    this._year = year;
-    this._location = location;
+// Define symbols for private attributes
+const _brand = Symbol('brand');
+const _motor = Symbol('motor');
+const _color = Symbol('color');
+
+// Define the Car class
+export default class Car {
+  constructor(brand, motor, color) {
+    // Initialize attributes with Symbols
+    this[_brand] = brand;
+    this[_motor] = motor;
+    this[_color] = color;
   }
 
-  get year() {
-    return this._year;
+  // Getter methods for accessing the private properties
+  get brand() {
+    return this[_brand];
   }
 
-  get location() {
-    return this._location;
+  get motor() {
+    return this[_motor];
+  }
+
+  get color() {
+    return this[_color];
+  }
+
+  // Method to clone the car object
+  cloneCar() {
+    return new this.constructor(
+      this[_brand],
+      this[_motor],
+      this[_color]
+    );
   }
 }
-
-export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
-  }
-
-  get fullName() {
-    return `${this._firstName} ${this._lastName}`;
-  }
-
-  get holbertonClass() {
-    return this._holbertonClass;
-  }
-
-  get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-  }
-}
-
-// instances
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
-
-export const listOfStudents = [student1, student2, student3, student4, student5];
