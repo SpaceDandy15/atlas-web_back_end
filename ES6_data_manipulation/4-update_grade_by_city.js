9b6aed9d0c65e0111a7e739d2b02b96d4def94c3
Malik Vance <malikovance@gmail.com>
2025-03-06 16:39:37 -0600

    lint

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,8 @@
 function updateStudentGradeByCity(students, city, newGrades) {
   return students
-    .filter(student => student.location === city)
-    .map(student => {
-      const grade = newGrades.find(gradeObj => gradeObj.studentId === student.id);
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const grade = newGrades.find((gradeObj) => gradeObj.studentId === student.id);
       return {
         ...student,
         grade: grade ? grade.grade : 'N/A',
