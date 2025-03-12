16ad6e86a181442e65867be582be91e43a2d02e0
Malik Vance <malikovance@gmail.com>
2025-03-12 14:01:35 -0500

    exporting list of students

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -40,3 +40,7 @@ const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+
+// Exporting the list of students
+export const listOfStudents = [student1, student2, student3, student4, student5];
