8d85f892d672435b9a672c84e1cf484a769b6bde
Malik Vance <malikovance@gmail.com>
2025-03-12 13:33:36 -0500

    lint fixes

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
@@ -8,7 +8,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   ]).then((results) =>
     results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.toString(), // Convert error to string
-    }))
+      value: result.status === 'fulfilled' ? result.value : result.reason.toString(),
+    })),
   );
 }
