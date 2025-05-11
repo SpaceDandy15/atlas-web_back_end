63f07cf03ac2156d68eb33cdfe4b39f157cd6f2c
Malik Vance <malikovance@gmail.com>
2025-05-10 20:36:18 -0500

    white space issues.

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,10 +7,10 @@ import random
 async def wait_random(max_delay: int = 10) -> float:
     """
     Waits for a random delay between 0 and max_delay and returns it.
-    
+
     Parameters:
     max_delay (int): Maximum delay in seconds (default 10).
-    
+
     Returns:
     float: The random delay.
     """
