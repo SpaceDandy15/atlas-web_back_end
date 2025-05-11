ceb1104a1b1a151e3195a5969b317337d534f504
Malik Vance <malikovance@gmail.com>
2025-05-10 20:56:23 -0500

    fixed pycode

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Defines `wait_n` coroutine to spawn `wait_random` n times and return delays 
+Defines `wait_n` coroutine to spawn `wait_random` n times and return delays
 in ascending order.
 """
 
@@ -11,11 +11,11 @@ from basic_async_syntax import wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Calls `wait_random` n times and returns delays in ascending order.
-    
+
     Parameters:
     n (int): Number of `wait_random` calls.
     max_delay (int): Maximum delay for each call.
-    
+
     Returns:
     List[float]: Sorted list of delays.
     """
