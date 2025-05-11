228cdb7a361af0e3c9afcc9a064f6ce4e9b2a39b
Malik Vance <malikovance@gmail.com>
2025-05-10 20:24:42 -0500

    task 9 was still too long.

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -10,9 +10,9 @@ def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     Returns a list of tuples containing elements and their lengths.
 
     Args:
-        lst (Iterable[Sequence]): Iterable of sequence items (like str, list, tuple).
+        Iterable of sequence items (like str, list, tuple).
 
     Returns:
-        List[Tuple[Sequence, int]]: List of tuples with each element and its length.
+        List[Tuple[Sequence, int]]: Tuples with each element and its length.
     """
     return [(i, len(i)) for i in lst]
