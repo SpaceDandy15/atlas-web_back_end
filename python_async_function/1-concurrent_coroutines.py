4f092a65492cfbf76824c6389c79fcf5fab0aa6d
Malik Vance <malikovance@gmail.com>
2025-05-10 20:57:40 -0500

    need extra blank line.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,7 @@ import asyncio
 from typing import List
 from basic_async_syntax import wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Calls `wait_random` n times and returns delays in ascending order.
