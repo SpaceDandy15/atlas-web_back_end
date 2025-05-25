1f86f2e882a06edbe0245c97af604ebe994e7471
Malik Vance <malikovance@gmail.com>
2025-05-25 17:25:39 -0500

    pycode fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -13,11 +13,13 @@ class BasicAuth(Auth):
     BasicAuth class that manages Basic Authentication.
     """
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
         """
         Extracts the Base64 part of the Authorization header.
         """
-        if authorization_header is None or not isinstance(authorization_header, str):
+        if authorization_header is None or notisinstance(authorization_header,
+                                                         str):
             return None
         if not authorization_header.startswith("Basic "):
             return None
