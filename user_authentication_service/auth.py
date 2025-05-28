eee1fc62dd440489b2e515209fe8c6eeda206cc2
Malik Vance <malikovance@gmail.com>
2025-05-27 22:33:07 -0500

    task 9

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,14 +4,15 @@ auth.py - Authentication service
 """
 
 import bcrypt
+import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 
 
-def _hash_password(password: str) -> bytes:
-    """Hashes a password using bcrypt."""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+def _generate_uuid() -> str:
+    """Generate a new UUID and return its string representation."""
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -19,6 +20,10 @@ class Auth:
 
     def __init__(self):
         self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Hashes a password using bcrypt."""
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
     def register_user(self, email: str, password: str) -> User:
         """
@@ -29,7 +34,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
-            hashed_pw = _hash_password(password)
+            hashed_pw = self._hash_password(password)
             return self._db.add_user(email, hashed_pw)
 
     def valid_login(self, email: str, password: str) -> bool:
@@ -39,7 +44,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
         except Exception:
             return False
