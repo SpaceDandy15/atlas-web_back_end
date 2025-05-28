fbba8d4da8a7d3100b39409f03d28de16642663a
Malik Vance <malikovance@gmail.com>
2025-05-28 09:07:22 -0500

    task 18 update password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -105,3 +105,17 @@ class Auth:
         reset_token = _generate_uuid()
         self._db.update_user(user.id, reset_token=reset_token)
         return reset_token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update the user's password using the reset token.
+        Raises ValueError if the user does not exist.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            raise ValueError("Invalid reset token")
+
+        hashed_pw = self._hash_password(password)
+        self._db.update_user(user.id,
+                             hashed_password=hashed_pw, reset_token=None)
