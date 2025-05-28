3cc7000d51e07ee30761282e155202395c2aa311
Malik Vance <malikovance@gmail.com>
2025-05-28 08:47:32 -0500

    blank line fix

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -79,7 +79,7 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-    
+
     def destroy_session(self, user_id: int) -> None:
         """
         Destroy a user's session by setting session_id to None.
