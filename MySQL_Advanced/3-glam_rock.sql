410775c5596a0dd45abc15c83fff29eb31f6cd90
Malik Vance <malikovance@gmail.com>
2025-06-08 15:03:59 -0500

    task 3

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,15 @@
+-- 3-glam_rock.sql
+-- List bands with Glam rock as main style, ranked by lifespan (years)
+
+SELECT
+    band_name,
+    CASE
+        WHEN split IS NULL OR split = 0 THEN YEAR(CURDATE()) - formed
+        ELSE split - formed
+    END AS lifespan
+FROM
+    metal_bands
+WHERE
+    main_genre = 'Glam rock'
+ORDER BY
+    lifespan DESC;
