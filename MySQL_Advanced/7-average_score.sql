1908353c7be9e4cf10d0ac71d658bdda67553ef4
Malik Vance <malikovance@gmail.com>
2025-06-08 15:35:18 -0500

    added comment

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,18 +1,16 @@
-DELIMITER $$
+-- Procedure to compute and update the average score for a given user
 
+DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
     DECLARE avg_score FLOAT;
-
     -- Calculate average score for the user
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
-
     -- Update average_score in users table
     UPDATE users SET average_score = avg_score WHERE id = user_id;
 END$$
-
 DELIMITER ;
