f9973edfba330a7bc0eee4bbc5f2cff0f10adc87
Malik Vance <malikovance@gmail.com>
2025-06-29 16:10:44 -0500

    task 5 tested and working.

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+// 5-payment.test.js
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleSpy;
+
+  beforeEach(() => {
+    consoleSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleSpy.restore();
+  });
+
+  it('should log "The total is: 120" when called with (100, 20)', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWithExactly('The total is: 120')).to.be.true;
+  });
+
+  it('should log "The total is: 20" when called with (10, 10)', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(consoleSpy.calledWithExactly('The total is: 20')).to.be.true;
+  });
+});
